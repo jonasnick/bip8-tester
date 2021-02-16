0872fd2f96fe6f3f9f27cbde027cbd39780b2793
Jonas Nick <jonasd.nick@gmail.com>
2021-02-16 15:26:40 +0000

    add result to threshold.py

--- threshold.py
+++ threshold.py
@@ -25,3 +25,8 @@ simulate(0.95, 2)
 simulate(0.95, 3)
 simulate(0.9, 2)
 simulate(0.9, 3)
+
+# Probability that unupdated node is on >= 2 block invalid fork with 95.0% threshold: 1.22%
+# Probability that unupdated node is on >= 3 block invalid fork with 95.0% threshold: 0.17%
+# Probability that unupdated node is on >= 2 block invalid fork with 90.0% threshold: 4.81%
+# Probability that unupdated node is on >= 3 block invalid fork with 90.0% threshold: 1.28%
